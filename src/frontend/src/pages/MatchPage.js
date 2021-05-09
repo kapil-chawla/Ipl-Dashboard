4283c4f420b670ec1052ec98468f4ed06b7e77c9
kapil-chawla <er.kapilchawla@gmail.com>
2021-05-09 16:02:12 +0530

    bind matchpage with backend

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -4,10 +4,25 @@ import { MatchSmallCard } from '../components/MatchSmallCard'
 import { useParams } from 'react-router-dom'
 
 export const MatchPage = () => {
-    
+    const[matches,setMatches] = useState([]);
+    const {teamName,year}  = useParams();
+    useEffect(
+        () => {
+            const fetchMatches = async() => {
+                const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                console.log(`data`, data);
+                setMatches(data);
+            }
+            fetchMatches();
+        }, [teamName,year]
+    );
     return (
         <div className="MatchPage">
             <h1>match page</h1>
+            {
+                matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
+            }
         </div>
     )
 }
